f39ef2ecd67ad74275d06565b5c406e2299bca18
R. Juanda Patra <patrajuanda10@gmail.com>
2025-12-18 22:47:21 +0700

    add

--- src/resources/views/admin/layouts/components/navbar.blade.php
+++ src/resources/views/admin/layouts/components/navbar.blade.php
@@ -80,7 +80,7 @@
       
       <li class="nav-item lh-1 me-3">
         
-            <a class="github-button" href="" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-admin-template-free on GitHub">Auth::user()->name </a>
+            <a class="github-button" href="" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star themeselection/sneat-html-admin-template-free on GitHub">{{Auth::user()->name}} </a>
             
 
 
